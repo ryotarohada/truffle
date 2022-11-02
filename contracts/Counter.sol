49a011ba61b45e002cf2bde25ef2d36178e196ab
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2022-11-02 13:10:23 +0900

    update

---
+++ contracts/Counter.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+  uint public count = 0;
+
+  function read() public view returns (uint) {
+    return count;
+  }
+
+  function increment() public {
+    count += 1;
+  }
+
+  function decrement() public {
+    count -= 1;
+  }
+
+  function add(uint value) public {
+    count += value;
+  }
+}
---
+++ migrations/1_deploy_contract.js
@@ -0,0 +1,7 @@
+const Counter = artifacts.require("Counter");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(Counter);
+  const value = await Counter.count;
+  console.log("Count of: ", value);
+};
