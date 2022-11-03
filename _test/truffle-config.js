71841d68aaa32bb78ef263640dff4c731597dda9
Ryotaro Hada <67140275+ryotarohada@users.noreply.github.com>
2022-11-03 23:16:39 +0900

    update

---
+++ _test/truffle-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    dashboard: {},
+    loc_development_development: {
+      network_id: "*",
+      port: 8545,
+      host: "127.0.0.1"
+    },
+    loc_7545_7545: {
+      network_id: "*",
+      port: 7545,
+      host: "127.0.0.1"
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.8.13"
+    }
+  },
+  db: {
+    enabled: false,
+    host: "127.0.0.1"
+  }
+};
